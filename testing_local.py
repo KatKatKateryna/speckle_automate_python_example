14313ef19cb9d13f96e830d34f4950d5581f12b1
KatKatKateryna <kateryna@speckle.systems>
2023-08-29 13:00:16 +0100

    add roads

--- testing_local.py
+++ testing_local.py
@@ -50,7 +50,7 @@ project_id = "17b0b76d13" #project_data.project_id
 version_id = "5d720c0998" #project_data.version_id
 
 #inputs.model_id = #project_data.model_id
-RADIUS = 100 #float(inputs.radius) 
+RADIUS = 500 #float(inputs.radius) 
 RESULT_BRANCH = "automate"
 
 account = get_local_accounts()[2]
@@ -248,7 +248,7 @@ def getBuildings(lat: float, lon: float):
                 except: tags.append( { 'building': rel_outer_ways_tags[n]['building']} )
         
         buildingsCount = len(ways)
-        print(buildingsCount)
+        #print(buildingsCount)
 
     # get coords of Ways
     objectGroup = []
@@ -282,6 +282,145 @@ def getBuildings(lat: float, lon: float):
         height = None   
     return objectGroup
 
+def joinRoads(coords: List[dict], closed: bool,  height: float):
+    from specklepy.objects.geometry import Polyline, Point 
+    points = []
+    
+    for i,c in enumerate(coords): 
+        points.append(Point.from_list([c['x'], c['y'], 0]))
+
+    poly = Polyline.from_points(points)
+    poly.closed = closed
+    poly.units = "m"
+    return poly
+
+
+def getRoads(lat: float, lon: float):
+    # https://towardsdatascience.com/loading-data-from-openstreetmap-with-python-and-the-overpass-api-513882a27fd0 
+    import requests
+    import json
+    keyword = "highway"
+
+    projectedCrs = createCRS(lat, lon)
+    lonPlus1, latPlus1 = reprojectToCrs(1, 1, projectedCrs, "EPSG:4326")
+    scaleX = lonPlus1 - lon
+    scaleY = latPlus1 - lat
+    r = RADIUS #meters
+
+    overpass_url = "http://overpass-api.de/api/interpreter"
+    overpass_query = f"""[out:json];
+    (node["{keyword}"]({lat-r*scaleY},{lon-r*scaleX},{lat+r*scaleY},{lon+r*scaleX});
+    way["{keyword}"]({lat-r*scaleY},{lon-r*scaleX},{lat+r*scaleY},{lon+r*scaleX});
+    relation["{keyword}"]({lat-r*scaleY},{lon-r*scaleX},{lat+r*scaleY},{lon+r*scaleX});
+    );out body;>;out skel qt;"""
+
+    response = requests.get(overpass_url, params={'data': overpass_query})
+    data = response.json()
+    features = data['elements']
+
+    ways = []
+    tags = []
+
+    rel_outer_ways = []
+    rel_outer_ways_tags = []
+
+    ways_part = []
+    nodes = []
+
+    for feature in features:
+        # ways
+        if feature['type'] == 'way':
+            try:
+                feature['id']
+                feature['nodes']
+                
+                tags.append( { f'{keyword}': feature['tags'][keyword] } )
+                ways.append( { 'id': feature['id'], 'nodes': feature['nodes'] } )
+            except:
+                ways_part.append( { 'id': feature['id'], 'nodes': feature['nodes'] } )
+        
+        # relations 
+        elif feature['type'] == 'relation':
+            outer_ways = []
+            outer_ways_tags = { f'{keyword}': feature['tags'][keyword] }
+            
+            for n, x in enumerate(feature['members']):
+                # if several Outer ways, combine them
+                if feature['members'][n]['type'] == 'way': # and feature['members'][n]['role'] == 'inner':
+                    outer_ways.append( { 'ref': feature['members'][n]['ref'] } )
+
+            rel_outer_ways.append( outer_ways )
+            rel_outer_ways_tags.append( outer_ways_tags )
+        
+        # get nodes (that don't have tags)
+        elif feature['type'] == 'node':
+            try: 
+                feature['tags']
+                feature['tags'][keyword]
+            except: 
+                #if feature['tags'][keyword] != 'traffic_signals':
+                nodes.append( { 'id': feature['id'], 'lat': feature['lat'], 'lon': feature['lon'] } )
+
+    # turn relations_OUTER into ways
+    for n, x in enumerate(rel_outer_ways):  
+        # there will be a list of "ways" in each of rel_outer_ways
+        full_node_list = []
+        for m, y in enumerate(rel_outer_ways[n]): 
+            #find ways_parts with corresponding ID
+            for k, z in enumerate(ways_part): 
+                if k == len(ways_part): break
+                if rel_outer_ways[n][m]['ref'] == ways_part[k]['id']:
+                    full_node_list += ways_part[k]['nodes'] 
+                    ways_part.pop(k) # remove used ways_parts
+                    k -= 1 # reset index
+                    break
+        
+            # move inside the loop to separate the sections
+            ways.append( { 'nodes': full_node_list } ) 
+            tags.append( { f'{keyword}': rel_outer_ways_tags[n][keyword] } )
+            # empty the list after each loop to start new part 
+            full_node_list = []
+        
+        roadsCount = len(ways)
+        #print(roadsCount)
+
+    # get coords of Ways
+    objectGroup = []
+    for i, x in enumerate(ways): # go through each Way: 2384
+        ids = ways[i]['nodes']
+        coords = [] # replace node IDs with actual coords for each Way
+        r'''
+        height = 3
+        tags[i][keyword]: height = 9
+        try: height = float( cleanString(tags[i]['levels'].split( ',' )[0].split( ';' )[0] )) * 3
+        except:
+            try: height = float( cleanString(tags[i]['height'].split( ',' )[0].split( ';' )[0]) )
+            except: 
+                try: 
+                    if tags[i]['layer'] < 0: height = -1 * height
+                except: pass
+        if height > 150: 
+            print(height)
+            #height = 10 
+        '''
+        closed = False
+        for k, y in enumerate(ids): # go through each node of the Way
+            if k==len(ids)-1 and y == ids[0]: 
+                closed = True
+                continue
+            for n, z in enumerate(nodes): # go though all nodes
+                if ids[k] == nodes[n]['id']: 
+                    x, y = reprojectToCrs(nodes[n]['lat'], nodes[n]['lon'], "EPSG:4326", projectedCrs)
+                    coords.append( { 'x': x, 'y': y } )
+                    break
+
+        obj = joinRoads( coords, closed, 0 )
+        objectGroup.append( obj )
+        coords = None
+        height = None   
+    return objectGroup
+
+
 try:
     import numpy as np 
     projInfo = base["info"] #[o for o in objects if o.speckle_type.endswith("Revit.ProjectInfo")][0] 
@@ -289,21 +428,31 @@ try:
     angle_deg = np.rad2deg(angle_rad)
     lon = np.rad2deg(projInfo["longitude"])
     lat = np.rad2deg(projInfo["latitude"])
+
+    #lat = 42.33868845652055
+    #lon = -71.08536785916132
+
     print(angle_rad)
     print(lon)
     print(lat)
 
-    blds = getBuildings(lat, lon)
     crsObj = None
+    commitObj = Collection(elements = [], units = "m", name = "Context", collectionType = "BuildingsLayer")
 
+    blds = getBuildings(lat, lon)
     bases = [Base(units = "m", displayValue = [b]) for b in blds]
-    commitObj = Collection(elements = bases, units = "m", name = "Context", collectionType = "Layer")
+    bldObj = Collection(elements = bases, units = "m", name = "Context", collectionType = "BuildingsLayer")
+    commitObj.elements.append(bldObj)
+    
+    roads = getRoads(lat, lon)
+    bases = roads
+    roadObj = Collection(elements = bases, units = "m", name = "Context", collectionType = "RoadsLayer")
+    commitObj.elements.append(roadObj)
     
     # create branch if needed 
     existing_branch = client.branch.get(project_id, RESULT_BRANCH, 1)  
     if existing_branch is None: 
         br_id = client.branch.create(stream_id = project_id, name = RESULT_BRANCH, description = "") 
-
 
     objId = send(commitObj, transports=[server_transport]) 
 
