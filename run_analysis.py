e8c48df4f33bebe6760043dbdb32ef77e5ec589c
KatKatKateryna <kateryna@speckle.systems>
2023-09-05 22:11:27 +0100

    remove scipy, add matplotlib and imports

--- run_analysis.py
+++ run_analysis.py
@@ -1,6 +1,15 @@
 
+import math
+from typing import List
+from specklepy.api.operations import receive, send
+from specklepy.objects.geometry import Line, Point, Pointcloud 
+from specklepy.objects.other import Collection
 
+import numpy as np 
 from operator import add, sub 
+import matplotlib as mpl
+
+from utils.utils_other import RESULT_BRANCH
 
 HALF_VIEW_DEGREES = 70
 STEP_DEGREES = 5
